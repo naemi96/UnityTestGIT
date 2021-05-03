13572305294c55c64a7b2ca0f4990aaeb096ea80
naemi96 <naemi.w.j@hotmail.com>
2021-05-03 10:53:50 +0200

    Update escape room

---
+++ MobHuset/Assets/Scripts/OpenURL1.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenURL1 : MonoBehaviour
+{
+    public void OpenURLradio()
+    {
+        Application.OpenURL("www.sverigesradio.se/avsnitt/1714113");
+    }
+}
